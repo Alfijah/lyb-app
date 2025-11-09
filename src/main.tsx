1dc1b43ffc86c1a8c61ad165bcccd0590eea8878
Alfijah Sarban <alfijah.sarban@ka.prorail.nl>
2025-11-09 22:15:42 +0100

    browserrouter removed

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,9 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 
