236bc3549bc42845e6286910a153d493668a1317
Alfijah Sarban <alfijah.sarban@ka.prorail.nl>
2025-11-09 22:02:35 +0100

    changes made

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
