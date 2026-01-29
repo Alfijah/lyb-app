0b969056ec355c6f549d0277a648bfc1075e9ecd
Alfijah <ajsarban@gmail.com>
2026-01-29 16:47:37 +0100

    routes changed and path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import HomePage from "./page";
