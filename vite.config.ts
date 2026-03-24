05bf15f911b2ffed576d52db5177a3b882dff6ba
Alfijah <ajsarban@gmail.com>
2026-03-24 17:38:29 +0100

    sitemap configs done

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    Sitemap({ 
-      hostname: 'https://drinklyb.com/',
-      dynamicRoutes: ['/about', '/menu', '/benefits', '/detoxen', '/faq'],
-      outDir: 'dist',
-      readable: true,
-      generateRobotsTxt: true,
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-  }
+  plugins: [react()],
+  base: '/', 
 })
